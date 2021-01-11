b7036010e041ba6211551bab02eebaa04c223cc6
viqbgrg <viqbgrg@gmail.com>
2021-01-11 11:00:39 +0800

    在main目录下也能使用单元测试

---
+++ java-onjava8/09_polymorphism/src/main/java/com/github/viqbgrg/onjava8/Polymorphism.java
@@ -0,0 +1,11 @@
+package com.github.viqbgrg.onjava8;
+
+import org.junit.jupiter.api.Test;
+
+public class Polymorphism {
+
+    @Test
+    public void test() {
+        System.out.println("haha");
+    }
+}
---
+++ java-onjava8/09_polymorphism/src/test/java/com/github/viqbgrg/onjava8/PolymorphismTest.java
@@ -0,0 +1,12 @@
+package com.github.viqbgrg.onjava8;
+
+import org.junit.jupiter.api.Test;
+
+
+class PolymorphismTest {
+
+    @Test
+    void test1() {
+        System.out.println("haha");
+    }
+}
