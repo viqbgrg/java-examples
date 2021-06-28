4bfc55a2476bd040ebc193d8216775af3ffdfada
viqbgrg <viqbgrg@gmail.com>
2021-06-28 10:56:01 +0800

    split

--- java-demo/demo/src/main/java/javademo/StringTest.java
+++ java-demo/demo/src/main/java/javademo/StringTest.java
@@ -1,6 +1,12 @@
 package javademo;
 
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class StringTest {
     void replaceTest() {
         String a = "ABCD";
@@ -8,5 +14,17 @@ public class StringTest {
         b.replace("a", "d");
         b.replace("d", "c");
         System.out.println(b);
+    }
+
+    @Test
+    void splitTest() {
+        String a = "a";
+        String b = "a, b";
+        String[] split = a.split(",");
+        String[] split1 = b.split(",");
+        List<String> collect = Arrays.asList(split).stream().map(String::trim).collect(Collectors.toList());
+        List<String> collect1 = Arrays.asList(split1).stream().map(String::trim).collect(Collectors.toList());
+        System.out.println(collect);
+        System.out.println(collect1);
     }
 }
