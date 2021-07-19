b1f5479c476592c4b6bad014b957b0af687beada
viqbgrg <viqbgrg@gmail.com>
2021-07-19 13:41:32 +0800

    Repeatable 多注解

--- java-demo/demo/src/main/java/javademo/clazz/ClazzTest.java
+++ java-demo/demo/src/main/java/javademo/clazz/ClazzTest.java
@@ -16,26 +16,38 @@ public class ClazzTest {
         Class<UserType> userClass = UserType.class;
         String simpleName = userClass.getSimpleName();
         System.out.println(simpleName);
-        final Field[] method = userClass.getDeclaredFields();
-        for (Field me : method) {
-            if (me.isAnnotationPresent(Key.class)) {
-                Key key = me.getAnnotation(Key.class);
-                String value = key.value();
-                System.out.println(value);
+        final Field[] fields = userClass.getDeclaredFields();
+        for (Field me : fields) {
+            if (me.getAnnotationsByType(Key.class).length != 0) {
+                Key[] key = me.getAnnotationsByType(Key.class);
+                for (int i = 0; i < key.length; i++) {
+                    Key key1 = key[i];
+                    String value = key1.value();
+                    System.out.println(value);
+                }
             }
         }
+    }
+
+    //    @Target(ElementType.FIELD)
+//    @Retention(RetentionPolicy.RUNTIME)
+//    @Documented
+    @Repeatable(Keys.class)
+    public @interface Key {
+        String value() default "";
     }
 
     @Target(ElementType.FIELD)
     @Retention(RetentionPolicy.RUNTIME)
     @Documented
-    public @interface Key {
-        String value() default "";
+    public @interface Keys {
+        Key[] value();
     }
 
     @Data
     private class UserType {
         @Key("test")
+        @Key("test1")
         private String username;
         private String password;
     }
