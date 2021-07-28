970bb994262902ba190f6d4565be8874ab27fa38
viqbgrg <viqbgrg@gmail.com>
2021-07-28 22:12:45 +0800

    stream iterator

--- java-demo/demo/src/main/java/javademo/stream/StreamTest.java
+++ java-demo/demo/src/main/java/javademo/stream/StreamTest.java
@@ -3,6 +3,7 @@ package javademo.stream;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -11,11 +12,25 @@ import java.util.stream.Collectors;
  * @author hhj
  */
 public class StreamTest {
+
+    List<String> strings = Arrays.asList("1", "2");
     @Test
     void streamTest() {
-        List<String> strings = Arrays.asList("1", "2");
         String collect = strings.stream().map(item -> "").filter(item -> !item.equals("")).collect(Collectors.joining(","));
         collect = Optional.of(collect).orElse("123333");
         System.out.println(collect);
+    }
+
+    @Test
+    void iteratorTest() {
+        Iterator<String> iterator = strings.stream().map(item -> {
+            System.out.println(item);
+            return item + "a";
+        }).iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            System.out.println(next);
+            break;
+        }
     }
 }
