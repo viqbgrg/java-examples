22367be28190e2ca490d019e490c9809fd0bc017
viqbgrg <viqbgrg@gmail.com>
2021-07-31 23:50:36 +0800

    方法引用

--- java-onjava8/functional/src/main/java/com/github/viqbgrg/onjava8/functional/UnboundMethodReference1.java
+++ java-onjava8/functional/src/main/java/com/github/viqbgrg/onjava8/functional/UnboundMethodReference1.java
@@ -1,13 +1,53 @@
 package com.github.viqbgrg.onjava8.functional;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
 /**
  * @author bing
  */
 class Y {
-
+    int show(){
+        System.out.println("方法执行");
+        return 1;
+    }
 }
-interface Test1 {
 
+class YY {
+
+    int a;
+
+    public YY(int a) {
+        this.a = a;
+    }
+
+    int show(YY yy){
+        System.out.printf("this = ", this.a);
+        System.out.println("yy.a = " + yy.a);
+        return this.a + yy.a;
+    }
+}
+@FunctionalInterface
+interface Test1 {
+    int execute(Y y);
+}
+@FunctionalInterface
+interface Test2 {
+    int execute(YY y, YY y1);
 }
 public class UnboundMethodReference1 {
+    @Test
+    void test() {
+        Test1 t = Y::show;
+        Y y = new Y();
+        int execute = t.execute(y);
+        System.out.println(execute);
+    }
+
+    @Test
+    void test1() {
+        Test2 test2 = YY::show;
+
+    }
 }
