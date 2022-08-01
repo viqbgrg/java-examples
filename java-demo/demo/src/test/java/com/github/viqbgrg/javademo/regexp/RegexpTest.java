f360af7b4127b85f2bea515a81a1db1a36ab9f58
viqbgrg <viqbgrg@gmail.com>
2022-08-01 10:44:52 +0800

    date 时区

--- java-demo/demo/src/test/java/com/github/viqbgrg/javademo/regexp/RegexpTest.java
+++ java-demo/demo/src/test/java/com/github/viqbgrg/javademo/regexp/RegexpTest.java
@@ -20,4 +20,17 @@ public class RegexpTest {
             System.out.println(mediaSourceId);
         }
     }
+
+    @Test
+    void test1() {
+        String resolution = "1920*1080";
+        String resolution1 = "1920x1036";
+        String resolution2 = "1920 x 1036";
+        Pattern sourcePattern = Pattern.compile("([0-9]{4}[*,x, ]{1,3}[0-9]{3,4})");
+        Matcher sourceMatcher = sourcePattern.matcher(resolution2);
+        if (sourceMatcher.find()) {
+            String group = sourceMatcher.group(1);
+            System.out.println(group);
+        }
+    }
 }
