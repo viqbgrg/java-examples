219c6a37356b8a5ac83e2cd480756ac67c88114e
viqbgrg <viqbgrg@gmail.com>
2023-04-11 23:41:00 +0800

    ip

--- java-demo/demo/src/main/java/javademo/net/HttpTest.java
+++ java-demo/demo/src/main/java/javademo/net/HttpTest.java
@@ -2,10 +2,19 @@ package javademo.net;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 public class HttpTest {
 
     @Test
-    public void http() {
-        System.out.println("http");
+    public void http() throws IOException {
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress("", 443), 2000);
+
     }
 }
